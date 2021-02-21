cff6131a2840110c0e7fc6471c9459fc6f85f7d9
raloubs <lobonala@privatam.com>
2021-02-21 18:15:14 +0100

    tet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,112 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div id="chartdiv"></div>
   </div>
 </template>
 
 <script>
+  import * as am4core from "@amcharts/amcharts4/core";
+  import * as am4maps from "@amcharts/amcharts4/maps";
+  import am4geodata_worldHigh from "@amcharts/amcharts4-geodata/worldHigh";
+  import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+  am4core.useTheme(am4themes_animated);
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted (){
+    // Create map instance
+    let chart = am4core.create("chartdiv", am4maps.MapChart);
+
+// Set map definition
+    chart.geodata = am4geodata_worldHigh;
+
+// Set projection
+    chart.projection = new am4maps.projections.Mercator();
+
+// Center on the groups by default
+    chart.homeZoomLevel = 6;
+    chart.homeGeoPoint = { longitude: 10, latitude: 51 };
+
+// Polygon series
+    let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    polygonSeries.exclude = ["AQ"];
+    polygonSeries.useGeodata = true;
+    polygonSeries.nonScalingStroke = true;
+    polygonSeries.strokeOpacity = 0.5;
+
+// Image series
+    let imageSeries = chart.series.push(new am4maps.MapImageSeries());
+    let imageTemplate = imageSeries.mapImages.template;
+    imageTemplate.propertyFields.longitude = "longitude";
+    imageTemplate.propertyFields.latitude = "latitude";
+    imageTemplate.nonScaling = true;
+
+    let image = imageTemplate.createChild(am4core.Image);
+    image.propertyFields.href = "imageURL";
+    image.width = 50;
+    image.height = 50;
+    image.horizontalCenter = "middle";
+    image.verticalCenter = "middle";
+
+    let label = imageTemplate.createChild(am4core.Label);
+    label.text = "{label}";
+    label.horizontalCenter = "middle";
+    label.verticalCenter = "top";
+    label.dy = 20;
+
+    imageSeries.data = [{
+      "latitude": 40.416775,
+      "longitude": -3.703790,
+      "imageURL": "https://www.amcharts.com/lib/images/weather/animated/rainy-1.svg",
+      "width": 32,
+      "height": 32,
+      "label": "Madrid: +22C"
+    }, {
+      "latitude": 48.856614,
+      "longitude": 2.352222,
+      "imageURL": "https://www.amcharts.com/lib/images/weather/animated/thunder.svg",
+      "width": 32,
+      "height": 32,
+      "label": "Paris: +18C"
+    }, {
+      "latitude": 52.520007,
+      "longitude": 13.404954,
+      "imageURL": "https://www.amcharts.com/lib/images/weather/animated/cloudy-day-1.svg",
+      "width": 32,
+      "height": 32,
+      "label": "Berlin: +13C"
+    }, {
+      "latitude": 52.229676,
+      "longitude": 21.012229,
+      "imageURL": "https://www.amcharts.com/lib/images/weather/animated/day.svg",
+      "width": 32,
+      "height": 32,
+      "label": "Warsaw: +22C"
+    }, {
+      "latitude": 41.872389,
+      "longitude": 12.480180,
+      "imageURL": "https://www.amcharts.com/lib/images/weather/animated/day.svg",
+      "width": 32,
+      "height": 32,
+      "label": "Rome: +29C"
+    }, {
+      "latitude": 51.507351,
+      "longitude": -0.127758,
+      "imageURL": "https://www.amcharts.com/lib/images/weather/animated/rainy-7.svg",
+      "width": 32,
+      "height": 32,
+      "label": "London: +10C"
+    }, {
+      "latitude": 59.329323,
+      "longitude": 18.068581,
+      "imageURL": "https://www.amcharts.com/lib/images/weather/animated/rainy-1.svg",
+      "width": 32,
+      "height": 32,
+      "label": "Stockholm: +8C"
+    } ];
   }
 }
 </script>
