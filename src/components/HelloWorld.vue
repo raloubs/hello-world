43a049952ebb2c3e07f0867a0a885f01f157ad78
raloubs <lobonala@privatam.com>
2021-02-21 18:21:30 +0100

    tet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <div id="chartdiv"></div>
   </div>
 </template>
@@ -113,6 +112,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  #chartdiv {
+    width: 70%;
+    height: 500px;
+  }
 h3 {
   margin: 40px 0 0;
 }
